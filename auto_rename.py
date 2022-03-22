09fa4e6b33cae6d0ef7c8aa8d127dbb620346fcb
Diptechis <dip@techis.io>
2022-03-22 19:54:14 +0530

    CSV Created

--- auto_rename.py
+++ auto_rename.py
@@ -4,15 +4,16 @@ import os
 # path = "/Users/musubimanagement/Desktop/image_dataset-main/with_mask"
 ss="/Users/musubimanagement/Desktop/image_dataset-main/Marged"
 
-val = 691
+val = 1
 
-# path = os.chdir("/Users/musubimanagement/Desktop/image_dataset-main/with_mask")
-path_without_mask = os.chdir("/Users/musubimanagement/Desktop/image_dataset-main/without_mask")
+path = os.chdir("/Users/musubimanagement/Desktop/image_dataset-main/with_mask")
+# path_without_mask = os.chdir("/Users/musubimanagement/Desktop/image_dataset-main/without_mask")
 for file in os.listdir():
     
     f_name, f_extention = os.path.splitext(file)
-    new_name_file = "pic{}{}".format(val,f_extention)     
-    os.rename(file,ss+"/"+new_name_file)    
+    new_name_file = "pic-{}{}".format(val,f_extention)     
+    # os.rename(file,ss+"/"+new_name_file)  
+    os.rename(file,new_name_file)  
     val += 1
     
    
